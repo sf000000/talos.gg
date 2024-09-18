567149e2def5fd7bbc1aa507bf62f28ad127aeb2
Jawad <chaptrhouse@gmail.com>
2024-09-17 21:13:39 -0500

    added theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
 import { cn } from "@/lib/utils";
-
+import { ThemeProvider } from "@/providers/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,8 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("antialiased", inter.className)}>
-        <Navbar />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
