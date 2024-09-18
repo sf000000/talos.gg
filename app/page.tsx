1b9888150ecb86a636f42f80799b2bcd3e8db475
Jawad <chaptrhouse@gmail.com>
2024-09-17 21:13:55 -0500

    disable recent repacks section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import RecentRepacks from "./components/recent-repacks";
+// import RecentRepacks from "./components/recent-repacks";
 import TrendingGames from "./components/trending-games";
 
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
       <TrendingGames />
-      <RecentRepacks />
+      {/* <RecentRepacks /> */}
     </div>
   );
 }
