452a6d1790ba6ad28cf56133ad9f5479ed5cd6a8
Jawad <chaptrhouse@gmail.com>
2024-09-18 20:25:52 -0500

    remove unused imports.

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -3,8 +3,6 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Badge } from "./ui/badge";
-import { Button } from "./ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
